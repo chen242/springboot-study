188daee6e192651c73547bbf07ae6cc425bd78b0
chenxiao <1945825785@qq.com>
2021-02-25 18:29:53 +0800

    处理的全局异常

--- src/main/java/com/jd/admin/exception/GlobalExceptionHandler.java
+++ src/main/java/com/jd/admin/exception/GlobalExceptionHandler.java
@@ -2,10 +2,17 @@ package com.jd.admin.exception;
 
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
+/*
+*
+*
+*
+* */
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler({ArithmeticException.class,NullPointerException.class})
     public String handleArithException(){
-        return "";
+        return "login";
     }
 }
